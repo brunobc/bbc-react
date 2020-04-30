9cd4d0af05a342e71ee378d2148f21bf7880065a
brunobc <brunobezerrachaves@gmail.com>
2020-04-30 15:49:37 -0300

    Set enzyme

--- src/components/ProgressBar/ProgressBar.spec.js
+++ src/components/ProgressBar/ProgressBar.spec.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import ProgressBar from './ProgressBar';
 
 describe('ProgressBar', () => {
+  configure({ adapter: new Adapter() });
+
   test('getWidthAsPercentOfTotalWidth should return 250 with total width of 500 and percent of 50', () => {
-      const wrapper = shallow(<ProgressBar percent={50} width={500} />);
-      const width = wrapper.instance().getWidthAsPercentOfTotalWidth();
-      expect(width).toEqual(250);
+    const wrapper = shallow(<ProgressBar percent={50} width={500} />);
+    const width = wrapper.instance().getWidthAsPercentOfTotalWidth();
+    expect(width).toEqual(250);
   });
 });
