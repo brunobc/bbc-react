5770d4d9b9b0baaee514b602e5af461738ef93e6
brunobc <brunobezerrachaves@gmail.com>
2020-04-30 23:13:22 -0300

    Updates

---
+++ precache-manifest.27fd1cfcfa22c48867b95dd788a3338c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd74127eb67ab394b2671ef79371727d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ab03a48f1fa222e431c",
+    "url": "/static/css/2.52738377.chunk.css"
+  },
+  {
+    "revision": "4a596289cc16b5fbdb64",
+    "url": "/static/css/main.a4200145.chunk.css"
+  },
+  {
+    "revision": "4ab03a48f1fa222e431c",
+    "url": "/static/js/2.85c3522d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.85c3522d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a596289cc16b5fbdb64",
+    "url": "/static/js/main.4eb078b6.chunk.js"
+  },
+  {
+    "revision": "a5954b74b95698decdf8",
+    "url": "/static/js/runtime-main.026e593c.js"
+  }
+]);
