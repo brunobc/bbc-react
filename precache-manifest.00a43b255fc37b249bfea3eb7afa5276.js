af27f9b54040e5884f4b7ef9b6d5bb1b98fb5f9e
brunobc <brunobezerrachaves@gmail.com>
2020-05-01 13:46:44 -0300

    Updates

---
+++ precache-manifest.00a43b255fc37b249bfea3eb7afa5276.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc10a6881a2120ed4986f78e1945f145",
+    "url": "/bbc-react/index.html"
+  },
+  {
+    "revision": "819bafb62d094d930d86",
+    "url": "/bbc-react/static/css/2.52738377.chunk.css"
+  },
+  {
+    "revision": "64d55ebbaf7e3670a775",
+    "url": "/bbc-react/static/css/main.a4200145.chunk.css"
+  },
+  {
+    "revision": "819bafb62d094d930d86",
+    "url": "/bbc-react/static/js/2.0204e186.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bbc-react/static/js/2.0204e186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d55ebbaf7e3670a775",
+    "url": "/bbc-react/static/js/main.82d9b973.chunk.js"
+  },
+  {
+    "revision": "d9485f727ccc9c7c900e",
+    "url": "/bbc-react/static/js/runtime-main.3e360e00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ds-react/precache-manifest.ac89541374a5f74d54745719cb03432c.js"
+  "/bbc-react/precache-manifest.00a43b255fc37b249bfea3eb7afa5276.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ds-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bbc-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
